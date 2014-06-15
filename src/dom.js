66f10e4d66a057806a5526f0c4db36c17be947a3
georapbox <georapbox@gmail.com>
2014-06-15 22:37:24 +0300

    Syntax errors

--- src/dom.js
+++ src/dom.js
@@ -4,7 +4,7 @@
  * @version 0.1.2
  * @author George Raptis | http://georap.gr
  */
-window.dom = window.dom || {};
+var dom = {};
 
 dom.classList = {
     /**
@@ -15,8 +15,8 @@ dom.classList = {
     contains: function(el, c) {
         'use strict';
 
-        if (arguments.length != 2) throw new Error('function "contains" called with ' + arguments.length + ' argument(s), but it expects 2 arguments.');
-        if (!el || !c) throw new Error('Can\'t check if class "' + c + '" is contained on ' + el);
+        if (arguments.length !== 2) { throw new Error('function "contains" called with ' + arguments.length + ' argument(s), but it expects 2 arguments.'); }
+        if (!el || !c) { throw new Error('Can\'t check if class "' + c + '" is contained on ' + el); }
 
         if (document.documentElement.classList) {
             this.contains = function(el, c) {
@@ -25,7 +25,7 @@ dom.classList = {
         } else {
             this.contains = function(el, c) {
                 var c_names = el.className.split(/\s+/),
-                    len = c_names.length >>> 0;
+                    len = c_names.length;
 
                 while (len--) {
                     if (c_names[len] === c) {
@@ -47,8 +47,8 @@ dom.classList = {
     add: function(el, c) {
         'use strict';
 
-        if (arguments.length != 2) throw new Error('function "add" called with ' + arguments.length + ' argument(s), but it expects 2 arguments.');
-        if (!el || !c) throw new Error('Can\'t add class "' + c + '" on ' + el);
+        if (arguments.length !== 2) { throw new Error('function "add" called with ' + arguments.length + ' argument(s), but it expects 2 arguments.'); }
+        if (!el || !c) { throw new Error('Can\'t add class "' + c + '" on ' + el); }
 
         if (document.documentElement.classList) {
             this.add = function(el, c) {
