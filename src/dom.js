07ebca73b74017996f64ca68e4ac32c28c215fdf
georapbox <georapbox@gmail.com>
2014-06-16 11:46:09 +0300

    classList.remove, classList.toggle, getElementsByClassName

--- src/dom.js
+++ src/dom.js
@@ -68,5 +68,112 @@ dom.classList = {
 
         this.add(el, c);
         return this;
+    },
+
+    /**
+     * @desc removes a CSS class from an element
+     * @param el - the element to remove the class from
+     * @param c (String) - the name of the class to remove from element (space seperated)
+     */
+    remove: function(el, c) {
+        'use strict';
+
+        if (arguments.length !== 2) { throw new Error('function "remove" called with ' + arguments.length + ' argument(s), but it expects 2 arguments.'); }
+        if (!el || !c) { throw new Error('Can\'t remove class "' + c + '" from ' + el); }
+
+        if (document.documentElement.classList) {
+            this.remove = function(el, c) {
+                el.classList.remove(c);
+                return this;
+            };
+        } else {
+            this.remove = function(el, c) {
+                var c_names = el.className.split(/\s+/),
+                    len = c_names.length;
+
+                while (len--) {
+                    if (c_names[len] === c) {
+                        c_names.splice(len, 1);
+                        el.className = c_names.join(' ');
+                        break;
+                    }
+                }
+
+                return this;
+            };
+        }
+
+        this.remove(el, c);
+        return this;
+    },
+
+    /**
+     * @desc toggles a class of an element
+     * @deps (fn) contains, (fn) remove, (fn) add
+     * @param el - the element to toggle its class
+     * @param c (string) - the class name
+     */
+    toggle: function(el, c) {
+        'use strict';
+
+        if (arguments.length !== 2) { throw new Error('function "toggle" called with ' + arguments.length + ' argument(s), but it expects 2 arguments.'); }
+        if (!el || !c) { throw new Error('Can\'t toggle class "' + c + '" on ' + el); }
+
+        if (document.documentElement.classList) {
+            this.toggle = function(el, c) {
+                el.classList.toggle(c);
+                return this;
+            };
+        } else {
+            this.toggle = function(el, c) {
+                if (this.contains(el, c)) {
+                    this.remove(el, c);
+                } else {
+                    this.add(el, c);
+                }
+                return this;
+            };
+        }
+
+        this.toggle(el, c);
+        return this;
     }
 };
+
+/**
+ * @desc get elements by class name
+ * @param node {Object} - the node to check for the class name (eg: document)
+ * @param c {String} - the class name
+ * @return node list {Array} with the elements that contain the class name
+ */
+dom.getElementsByClassName = function (node, c) {
+    'use strict';
+
+    // if getElementsByClassName is supported
+    if (document.getElementsByClassName) {
+        return node.getElementsByClassName(c);
+    }
+
+    // if getElementsByClassName is NOT supported but querySelectorAll is supported
+    if (!document.getElementsByClassName && document.querySelectorAll) {
+        return node.querySelectorAll('.' + c);
+    }
+
+    // if neither getElementsByClassName nor querySelectorAll is supported
+    if (!document.getElementsByClassName && !document.querySelectorAll) {
+        var arr = [],
+            regx = new RegExp('(^| )' + c + '( |$)'),
+            els = node.getElementsByTagName('*'),
+            i = 0,
+            len = els.length,
+            el;
+
+        for (; i < len; i++) {
+            el = els[i];
+            if (regx.test(el.className)) {
+                arr.push(el);
+            }
+        }
+        return arr;
+    }
+}
